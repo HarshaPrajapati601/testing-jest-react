2ea155a2cf9cca053cc25946e3dd061999d5aea3
Harsha Prajapati <harsha.prajapati@scripbox.com>
2021-12-19 12:12:04 +0530

    Integration basic setup

---
+++ src/__tests__/intergration.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Root from 'Root';
+import App  from 'components/App';
+
+it('Can fetch a list of comments and display them', ()=> {
+    //Atempts to render the entire app
+
+    //find the fetch comment button and click it
+
+    //expect to find a list of commetns i.e 500 commnets for ex
+});
