34d8ca0c173c2460122251bfd585acf1026b941c
Harsha Prajapati <harsha.prajapati@scripbox.com>
2021-12-26 17:21:08 +0530

    Else logic promise

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -6,12 +6,26 @@
 // //middlewares are series of functions that return each other.
 //total 3 funcuton that returns a fn and that in urn returns a fn
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ dispatch}) =>  next => action => {    
     //middleware logic
     //check to see if the action has a promise on its 'payload' property 
     //if it does then wait for it to resolve
     // if it doesnot then sent the action to the 
     //next middleware
+    //if the action has payload and it has a promise
+    if(!action.payload || !action.payload.then ) {
+        //to forward the action to next middleware of pur chain
+        return next(action);
+    }
+    //if it s a promise then we want to wait for  its data 
+    //..and create a new action with the data 
+    //and dispatch it
+    action.payload.then(function(response) {
+        const newAction = { ...action, payload: response};
+        dispatch(newAction)
+    })
+
         }
 
 //middlewares are series of functions that return each other.
