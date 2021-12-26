bd0b61ae8f166beec2dfa10c84514d235c58c098
Harsha Prajapati <harsha.prajapati@scripbox.com>
2021-12-26 17:09:01 +0530

    Refactor of funcitons

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,9 +1,13 @@
-export default function({ dispatch}) { //called with diff properties of obj
-    return function(next){ //next is a ref to the next middleware inside of our chain to  forward the action to next middleware
-        return function(action) {
+// export default ({ dispatch}) =>  //called with diff properties of obj
+//      next =>  //next is a ref to the next middleware inside of our chain to  forward the action to next middleware
+//         action => {        
+//         }
+
+// //middlewares are series of functions that return each other.
+//total 3 funcuton that returns a fn and that in urn returns a fn
+
+export default ({ dispatch}) =>  next => action => {    
 
         }
-    }
-}
-//middlewares are series of functions that return each other.
 
+//middlewares are series of functions that return each other.
